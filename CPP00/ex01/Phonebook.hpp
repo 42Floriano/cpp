aaeb78535d91b06f633a1ceff127ea97ccbaec46
Floriano Albertini <floriano.webdev@gmail.com>
2024-11-06 15:30:50 +0100

    Backup

---
+++ CPP00/ex01/Phonebook.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Phonebook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/06 14:37:08 by falberti          #+#    #+#             */
+/*   Updated: 2024/11/06 15:13:27 by falberti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+
+# define PHONEBOOK_HPP
+# include <iostream>
+# include <string>
+# include "Contact.hpp"
+
+class Phonebook
+{
+    private:
+        Contact contact[8];
+        int     _index;
+    public:
+        
+};
+
+#endif
