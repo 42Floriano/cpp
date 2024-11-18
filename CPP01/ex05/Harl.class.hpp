6ee333c33827c0bbaf8f4b8b9dc4e92665b53147
Floriano Albertini <floriano.webdev@gmail.com>
2024-11-18 12:22:44 +0100

    backup

---
+++ CPP01/ex05/Harl.class.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.class.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/18 09:33:04 by albertini         #+#    #+#             */
+/*   Updated: 2024/11/18 12:21:59 by falberti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_CLASS_HAPP
+# define HARL_CLASS_HAPP
+
+# include <iostream>
+# include <string>
+
+# define ARGS 0
+# define EMPTY 1
+# define INPUT 2
+# define OUTPUT 3
+
+#endif
