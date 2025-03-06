348889a8c573c70d2de41dca5481650ea98e1b10
Floriano Albertini <floriano.webdev@gmail.com>
2025-03-06 17:31:09 +0100

    backup

---
+++ CPP06/ex00/ScalarConverter.class.cpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.class.cpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 17:28:11 by falberti          #+#    #+#             */
+/*   Updated: 2025/03/06 17:29:28 by falberti         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALARCONVERTER_CLASS_HPP
+#define SCALARCONVERTER_CLASS_HPP
+
+class ScalarConverter{
+  private:
+
+  public:  
+};
+
+#endif
