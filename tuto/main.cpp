907d270f28adc95e7615f7e51ce3a23cd4cef8c1
Floriano Albertini <floriano@florianos-macbook-pro.home>
2025-05-09 15:04:40 +0200

    backup

--- tuto/main.cpp
+++ tuto/main.cpp
@@ -174,6 +174,13 @@ std::ostream & operator<<(std::ostream& out, const Polygone& obj){
 
 int main(void){
 
+    std::cout << "This is the line number " << __LINE__;
+    std::cout << " of file " << __FILE__ << ".\n";
+    std::cout << "Its compilation began " << __DATE__;
+    std::cout << " at " << __TIME__ << ".\n";
+    std::cout << "The compiler gives a __cplusplus value of ";
+    std::cout << __cplusplus;
+    //return 0; 
     Square sqr;
     Polygone *psquare;
     psquare = &sqr;
